5a6c75db2779022cd11c32ec6c9342138b7b5686
Jorn van Wier <mail@jornvanwier.com>
2022-01-25 21:33:28 +0100

    Add baseline model

--- task2.py
+++ task2.py
@@ -1,8 +1,8 @@
-from typing import Tuple
+from typing import Tuple, Any
 
 import numpy as np
 import pandas as pd
-from sklearn import model_selection, semi_supervised, metrics
+from sklearn import model_selection, semi_supervised, metrics, linear_model
 
 
 def load_data() -> Tuple[np.ndarray, np.ndarray]:
@@ -14,6 +14,17 @@ def load_data() -> Tuple[np.ndarray, np.ndarray]:
     return values.to_numpy(), labels.to_numpy()
 
 
+def evaluate_model(model: Any, x_train: np.ndarray, y_train: np.ndarray, x_test: np.ndarray, y_test: np.ndarray) -> \
+        Tuple[float, float]:
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    accuracy = metrics.accuracy_score(y_test, y_pred)
+    f1 = metrics.f1_score(y_test, y_pred)
+    # print(metrics.confusion_matrix(y_test, y_pred))
+    print(f'{f1=:.3f}\t{accuracy=:.3f}')
+    return accuracy, f1
+
+
 def main():
     print('Loading data')
     data, labels = load_data()
@@ -22,22 +33,24 @@ def main():
     x_train_lab, x_train_unlab, y_train_lab, _ = model_selection.train_test_split(x_train, y_train, test_size=.7,
                                                                                   stratify=y_train)
 
-    # TODO train baseline model here with only x_train_lab
+    print('Baseline model')
+    baseline = linear_model.LogisticRegression()
+    evaluate_model(baseline, x_train_lab, y_train_lab, x_test, y_test)
 
     # Same as previous x_train, but shuffled by train_test_split
     x_train = np.concatenate((x_train_lab, x_train_unlab))
     y_train_combined = np.concatenate((y_train_lab, np.full((x_train_unlab.shape[0],), -1)))
 
-    print('Propagating labels')
-    model = semi_supervised.LabelPropagation(kernel='knn', n_jobs=-1)
-    model.fit(x_train, y_train_combined)
-    semi_f1 = metrics.f1_score(y_test, model.predict(x_test))
-    print('Semi-supervised F1', semi_f1)
+    print('Semi-supervised')
+    semi_model = semi_supervised.LabelPropagation(kernel='knn', n_jobs=-1)
+    evaluate_model(semi_model, x_train, y_train_combined, x_test, y_test)
 
     # TODO is this correct?
-    y_train = np.concatenate((y_train_lab, model.transduction_))
+    y_train = semi_model.transduction_
 
-    # TODO retrain baseline with new y_train
+    print('Baseline with complete dataset')
+    baseline_complete = linear_model.LogisticRegression()
+    evaluate_model(baseline_complete, x_train, y_train, x_test, y_test)
 
 
 if __name__ == '__main__':
