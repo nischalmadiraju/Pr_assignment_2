db67ec6fbce772ccba4cd949f2f4c5704a441f2d
NBurgler <63599473+NBurgler@users.noreply.github.com>
2022-01-27 17:33:30 +0100

    Final version

--- Big_Cats_Pipeline.py
+++ Big_Cats_Pipeline.py
@@ -17,7 +17,7 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.neighbors import NearestNeighbors
 
 
-def edge_detection(img):
+def edge_detection(img):    # Edge detection for Fourier transform
     # Blur the image for better edge detection
     img_blur = cv2.GaussianBlur(img, (3, 3), 0)
 
@@ -34,7 +34,7 @@ def fourier_transform(img):
     return descriptors
 
 
-def create_dictionary(X_train, k_fold):
+def create_dictionary(X_train, k_fold): # Create a dictionary of keypoints, the best 10 keypoints for each image
     if k_fold == 0:
         dictionary = np.zeros((1270, 128), dtype=int)
     else:
@@ -53,7 +53,7 @@ def create_dictionary(X_train, k_fold):
     return dictionary
 
 
-def calculate_feature_vectors(X_train, X_test):
+def calculate_feature_vectors(X_train, X_test):  # Create SIFT feature vectors by comparing keypoints to the dictionary
     trainDataFeatures = []
     sift = cv2.SIFT_create()
     for image in X_train:
@@ -90,20 +90,40 @@ def calculate_feature_vectors(X_train, X_test):
     return trainDataFeatures, testDataFeatures
 
 
-def do_classification(X_train, y_train, X_test, y_test, model):
+def do_classification(X_train, y_train, X_test, y_test, featureExtraction, model): # Use the classification algorithms
     accuracy = 0
     if model == 0:
-        KNN = KNeighborsClassifier()
+        if featureExtraction == 0:    # No feature extraction
+            KNN = KNeighborsClassifier(algorithm='ball_tree', n_neighbors=9, p=1)
+        elif featureExtraction == 1:  # SIFT
+            KNN = KNeighborsClassifier(algorithm='ball_tree', n_neighbors=5, p=2)
+        elif featureExtraction == 2:  # Fourier Transform
+            KNN = KNeighborsClassifier(algorithm='ball_tree', n_neighbors=3, p=1)
+
         KNN.fit(X_train, y_train)
         KNNPredictions = KNN.predict(X_test)
         accuracy = metrics.accuracy_score(y_test, KNNPredictions)
+
     elif model == 1:
-        randomForest = RandomForestClassifier(n_estimators=100)
+        if featureExtraction == 0:    # No feature extraction
+            randomForest = RandomForestClassifier(max_depth=100, min_samples_leaf=2, min_samples_split=4, n_estimators=50)
+        elif featureExtraction == 1:  # SIFT
+            randomForest = RandomForestClassifier(max_depth=50, min_samples_leaf=1, min_samples_split=2, n_estimators=100)
+        elif featureExtraction == 2:  # Fourier Transform
+            randomForest = RandomForestClassifier(max_depth=10, min_samples_leaf=1, min_samples_split=2, n_estimators=10)
+
         randomForest.fit(X_train, y_train)
         randomForestPredictions = randomForest.predict(X_test)
         accuracy = metrics.accuracy_score(y_test, randomForestPredictions)
+
     elif model == 2:
-        logisticRegression = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=10000)
+        if featureExtraction == 0:    # No feature extraction
+            logisticRegression = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=10000, C=0.5, tol=0.01)
+        elif featureExtraction == 1:  # SIFT
+            logisticRegression = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=10000, C=1.0, tol=0.01)
+        elif featureExtraction == 2:  # Fourier Transform
+            logisticRegression = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=10000, C=0.5, tol=0.01)
+
         logisticRegression.fit(X_train, y_train)
         logisticRegressionPredictions = logisticRegression.predict(X_test)
         accuracy = metrics.accuracy_score(y_test, logisticRegressionPredictions)
@@ -116,19 +136,18 @@ def do_grid_search(X_train, y_train):
                                 'algorithm': ['ball_tree', 'kd_tree', 'brute'],
                                 'p': [1, 2, 3]}
 
-    RF_hyperparameter_space = {'n_estimators': [5, 10, 25, 50, 100],
-                               'criterion': ["gini", "entropy"],
-                               'max_depth': [10, 50, 100, None],
-                               'min_samples_split': [2, 3, 4, 5, 10],
-                               'min_samples_leaf': [1, 2, 3, 4, 5],
-                               'max_features': ["auto", "log2", None],
-                               'bootstrap': [True, False]}
+    RF_hyperparameter_space = {'n_estimators': [10, 50, 100],
+                               'criterion': ["gini"],
+                               'max_depth': [10, 50, 100],
+                               'min_samples_split': [2, 4, 6],
+                               'min_samples_leaf': [1, 2, 3],
+                               'bootstrap': [True]}
 
     LR_hyperparameter_space = {'tol': [0.01, 0.001],
                                'C': [0.5, 1.0, 1.5]}
 
-    KNN = KNeighborsClassifier(n_jobs=-1)
-    KNNGridSearch = GridSearchCV(KNN, param_grid=KNN_hyperparameter_space, scoring='accuracy', n_jobs=-1, cv=3)
+    KNN = KNeighborsClassifier(n_jobs=4)
+    KNNGridSearch = GridSearchCV(KNN, param_grid=KNN_hyperparameter_space, scoring='accuracy', n_jobs=4, cv=3)
     KNNGridSearch.fit(X_train, y_train)
     print("Optimal KNN hyperparameter combination:", KNNGridSearch.best_params_)
     print("Mean cross-validated training accuracy score:", KNNGridSearch.best_score_)
@@ -156,8 +175,8 @@ if __name__ == "__main__":
 
     clustering = 0  # 1 for clustering, 0 for no clustering
     k_fold = 0  # 1 for k-fold cross-validation, 0 for train-test-split
-    featureExtraction = 2  # 0 for no feature extraction, 1 for SIFT, 2 for Fourier Transform
-    gridSearch = 1  # 1 for grid search, 0 for no grid search
+    featureExtraction = 0  # 0 for no feature extraction, 1 for SIFT, 2 for Fourier Transform
+    gridSearch = 0  # 1 for grid search, 0 for no grid search. Only works when k_fold is 0
 
     silhouette = 0
     idx = 0
@@ -175,13 +194,13 @@ if __name__ == "__main__":
                     displayImage = cv2.resize(image, (50, 50))  # dimension reduction, otherwise silhouette score
                     clusterImage = displayImage.reshape(-1, 3)  # takes too long
 
-                    # neigh = NearestNeighbors(n_neighbors=10)    # This can be uncommented to determine the value of eps
-                    # nbrs = neigh.fit(clusterImage)
-                    # distances, indices = nbrs.kneighbors(clusterImage)
-                    # distances = np.sort(distances, axis=0)
-                    # distances = distances[:, 1]
-                    # plt.plot(distances)
-                    # plt.show()
+                    #neigh = NearestNeighbors(n_neighbors=10)    # This can be uncommented to determine the value of eps
+                    #nbrs = neigh.fit(clusterImage)
+                    #distances, indices = nbrs.kneighbors(clusterImage)
+                    #distances = np.sort(distances, axis=0)
+                    #distances = distances[:, 1]
+                    #plt.plot(distances)
+                    #plt.show()
 
                     db = DBSCAN(eps=4.5, min_samples=10).fit(clusterImage[:, :2])
 
@@ -211,37 +230,58 @@ if __name__ == "__main__":
 
             else:
                 continue
+
     if clustering == 1:
         print("Mean silhouette score:", silhouette / 170)
 
-    if k_fold == 1:
+    if k_fold == 1:  # k-fold cross validation
         dataLabels = np.array(dataLabels)
         k_folds = KFold(n_splits=3, shuffle=True)
         KNNAccuracy = 0
         randomForestAccuracy = 0
         logisticRegressionAccuracy = 0
-        for train_index, test_index in k_folds.split(dataImages, dataLabels):
-            X_train, X_test = dataImages[train_index], dataImages[test_index]
-            y_train, y_test = dataLabels[train_index], dataLabels[test_index]
+        if featureExtraction == 2:
+            for train_index, test_index in k_folds.split(fourierFeatures, dataLabels):
+                X_train, X_test = fourierFeatures[train_index], fourierFeatures[test_index]
+                y_train, y_test = dataLabels[train_index], dataLabels[test_index]
 
-            if featureExtraction == 1:
-                #  Create keypoint dictionary (10 keypoints per image) using only the training data
-                dictionary = create_dictionary(X_train, k_fold)
+                if featureExtraction == 1:
+                    #  Create keypoint dictionary (10 keypoints per image) using only the training data
+                    dictionary = create_dictionary(X_train, k_fold)
 
-                # Calculate feature vectors for the training and testing data
-                X_train, X_test = calculate_feature_vectors(X_train, X_test)
+                    # Calculate feature vectors for the training and testing data
+                    X_train, X_test = calculate_feature_vectors(X_train, X_test)
 
-            # Classification
-            KNNAccuracy += do_classification(X_train, y_train, X_test, y_test, 0)
-            randomForestAccuracy += do_classification(X_train, y_train, X_test, y_test, 1)
-            logisticRegressionAccuracy += do_classification(X_train, y_train, X_test, y_test, 2)
+                # Classification
+                KNNAccuracy += do_classification(X_train, y_train, X_test, y_test, featureExtraction, 0)
+                randomForestAccuracy += do_classification(X_train, y_train, X_test, y_test, featureExtraction, 1)
+                logisticRegressionAccuracy += do_classification(X_train, y_train, X_test, y_test, featureExtraction, 2)
+        else:
+            for train_index, test_index in k_folds.split(dataImages, dataLabels):
+                X_train, X_test = dataImages[train_index], dataImages[test_index]
+                y_train, y_test = dataLabels[train_index], dataLabels[test_index]
+
+                if featureExtraction == 1:
+                    #  Create keypoint dictionary (10 keypoints per image) using only the training data
+                    dictionary = create_dictionary(X_train, k_fold)
+
+                    # Calculate feature vectors for the training and testing data
+                    X_train, X_test = calculate_feature_vectors(X_train, X_test)
+
+                # Classification
+                KNNAccuracy += do_classification(X_train, y_train, X_test, y_test, featureExtraction, 0)
+                randomForestAccuracy += do_classification(X_train, y_train, X_test, y_test, featureExtraction, 1)
+                logisticRegressionAccuracy += do_classification(X_train, y_train, X_test, y_test, featureExtraction, 2)
 
         print("Mean accuracy for KNN:", KNNAccuracy / 3)
         print("Mean accuracy for Random Forest:", randomForestAccuracy / 3)
         print("Mean accuracy for Logistic Regression:", logisticRegressionAccuracy / 3)
 
-    else:
-        X_train, X_test, y_train, y_test = train_test_split(dataImages, dataLabels, random_state=9)
+    else:  # train-test split
+        if featureExtraction == 2:
+            X_train, X_test, y_train, y_test = train_test_split(fourierFeatures, dataLabels)
+        else:
+            X_train, X_test, y_train, y_test = train_test_split(dataImages, dataLabels)
 
         if featureExtraction == 1:
             dictionary = create_dictionary(X_train, k_fold)  # Create keypoint dictionary using only the training data
@@ -253,6 +293,6 @@ if __name__ == "__main__":
             do_grid_search(X_train, y_train)
 
         # Classification
-        print("Accuracy for KNN:", do_classification(X_train, y_train, X_test, y_test, 0))
-        print("Accuracy for Random Forest:", do_classification(X_train, y_train, X_test, y_test, 1))
-        print("Accuracy for Logistic Regression:", do_classification(X_train, y_train, X_test, y_test, 2))
+        print("Accuracy for KNN:", do_classification(X_train, y_train, X_test, y_test, featureExtraction, 0))
+        print("Accuracy for Random Forest:", do_classification(X_train, y_train, X_test, y_test, featureExtraction, 1))
+        print("Accuracy for Logistic Regression:", do_classification(X_train, y_train, X_test, y_test, featureExtraction, 2))
